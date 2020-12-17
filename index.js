4f6183b5fa8108dba4ba0b9af121e76c19aa6bdd
Sonja Ortmann <so.ortmann@gmail.com>
2020-12-17 14:15:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-var x = myFunction(1, 80);
-function MyFunction (a, b) {return a += b;}
-function subtract(60-40)
-function multiply(2*3.4)
-function divide(5.0/2.5)
+function add(a,b) {return a +b;}
